6b6f02ca329a04cd5e6a910f77bde8fb0481fc23
Antonio_Moreno <gamamurder@gmail.com>
2024-09-18 07:48:36 -0600

    Parcial01 - AddDTOs

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> RegisterAsync(RegisterRequest request)
+    {
+        if (await UserExistsAsync(request.Username)) 
+        { 
+            return BadRequest("Username already registered"); 
+        } //SI NO ENCUENTRA COINICIDENCIAS, MANDA 
+        //UN BAD REQUEST
+        using var hmac = new HMACSHA512();
+        var user = new AppUser
+        {
+            UserName = request.Username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
+            PasswordSalt = hmac.Key
+        };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        return user;
+    }
+    private async Task<bool> UserExistsAsync(string username) =>
+        await context.Users.AnyAsync(
+            u => u.UserName.ToLower() == username.ToLower()
+        ); //PERMITE BUSCAR LA PRIMER COINCIDENCIA EN NOMBRES DE USUARIO
+
+}
