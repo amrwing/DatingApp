52a02aaca2e761e9f4ec401f6fa9a65ae998b9ac
Antonio_Moreno <gamamurder@gmail.com>
2024-09-26 07:34:49 -0600

    Parcial01 - Add bootstrap dropdown

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { NgIf } from '@angular/common';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule,NgIf],
+  imports: [FormsModule,BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
@@ -25,5 +25,8 @@ not: any;
             }
         });
         
+    }
+    logout():void{
+        this.loggedIn = false;
     }
 }
