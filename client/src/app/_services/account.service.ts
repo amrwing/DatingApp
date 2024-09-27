fb62d39dfa8bb73add91d14bab3dc1e179d6921e
Antonio_Moreno <gamamurder@gmail.com>
2024-09-27 07:05:54 -0600

    Parcial01 - Implemented login and user model

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,13 +1,26 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable, signal } from '@angular/core';
+import { map, Observable, take } from 'rxjs';
+import { User } from '../models/User';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
   baseUrl = "https://localhost:5001/api/";
-  login(model: any): Observable<Object> {
-    return this.http.post(this.baseUrl + "account/login", model);
+  currentUser = signal<User | null>(null);
+
+  login(model: any){
+    return this.http.post<User>(this.baseUrl + "account/login", model).pipe(map(user => {
+      if(user){
+        localStorage.setItem("user",JSON.stringify(user));
+        this.currentUser.set(user);
+      }
+    }));
+  }
+  logout(){
+    localStorage.removeItem("user");
+    this.currentUser.set(null);
   }
 }
