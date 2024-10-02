234c6bb2f1db9e858175002ba87dcf1ec4740bc9
Antonio_Moreno <gamamurder@gmail.com>
2024-10-02 07:52:20 -0600

    Parcial01 - Add register components

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, input} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  usersFromHomeComponent = input.required<any>();
+  model: any = {};
+  register():void{
+    console.log(this.model);
+  }
+  cancel(): void{
+    console.log("Cancel");
+  }
+}
