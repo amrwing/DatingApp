fc86c15b42fa33d432a89aa50da48d2ac7512f0f
Antonio_Moreno <gamamurder@gmail.com>
2024-10-08 07:37:35 -0600

    Parcial02 - Add notification service

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-nav',
   standalone: true,
@@ -13,7 +14,7 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 export class NavComponent {
      accountService = inject(AccountService);
      private router = inject(Router);
-   
+     private toastr = inject(ToastrService);
     model: any = {};
 not: any;
     login():void{
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
   private accountService = inject(AccountService);
   //usersFromHomeComponent = input.required<any>();
   cancelRegister = output<boolean>();
+  private toastr = inject(ToastrService);
   model: any = {};
   register():void{
    this.accountService.register(this.model).subscribe({
@@ -22,6 +24,7 @@ export class RegisterComponent {
       },
       error: (error)=>{
         console.log(error);
+        this.toastr.error(error.error);
       }
     });
   }
