481aff2725d0fd310303c2d3936aa4f019e30c80
Antonio_Moreno <gamamurder@gmail.com>
2024-10-10 07:48:53 -0600

    Parcial02 - Add editorconfig

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+namespace API.Controllers;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
@@ -6,8 +7,6 @@ using API.Entities;
 using API.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
-namespace API.Controllers;
 
 public class AccountController(
     DataContext context,
@@ -18,9 +17,9 @@ public class AccountController(
     {
         if (await UserExistsAsync(request.Username))
         {
-            return BadRequest("Username already registered");
-        } //SI NO ENCUENTRA COINICIDENCIAS, MANDA 
-        //UN BAD REQUEST
+            return BadRequest("Username already in use");
+        }
+
         using var hmac = new HMACSHA512();
         var user = new AppUser
         {
@@ -28,39 +27,45 @@ public class AccountController(
             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
             PasswordSalt = hmac.Key
         };
+
         context.Users.Add(user);
         await context.SaveChangesAsync();
+
         return new UserResponse
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user)
         };
     }
-    private async Task<bool> UserExistsAsync(string username) =>
-        await context.Users.AnyAsync(
-            u => u.UserName.ToLower() == username.ToLower()
-        ); //PERMITE BUSCAR LA PRIMER COINCIDENCIA EN NOMBRES DE USUARIO
+
     [HttpPost("login")]
     public async Task<ActionResult<UserResponse>> LoginAsync(LoginRequest request)
     {
         var user = await context.Users.FirstOrDefaultAsync(x => x.UserName.ToLower() == request.Username.ToLower());
+
         if (user == null)
+        {
             return Unauthorized("Invalid username or password");
+        }
 
         using var hmac = new HMACSHA512(user.PasswordSalt);
         var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
-        for (int i = 0; i < computeHash.Length; i++)
+
+        for (var i = 0; i < computeHash.Length; i++)
         {
             if (computeHash[i] != user.PasswordHash[i])
             {
-                return Unauthorized("Invalid  or password");
-
+                return Unauthorized("Invalid username or password");
             }
         }
-                return new UserResponse
+
+        return new UserResponse
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user)
         };
     }
+
+    private async Task<bool> UserExistsAsync(string username) =>
+        await context.Users.AnyAsync(u => u.UserName.ToLower() == username.ToLower());
 }
