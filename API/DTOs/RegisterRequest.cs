cf206f35a3a8f5afab0595754a0dc84fdc4d8e63
Antonio_Moreno <gamamurder@gmail.com>
2024-10-11 07:28:41 -0600

    Parcial02 - Add buggy controller to test errores

--- API/DTOs/RegisterRequest.cs
+++ API/DTOs/RegisterRequest.cs
@@ -1,13 +1,12 @@
+namespace API.DTOs;
 
 using System.ComponentModel.DataAnnotations;
 
-namespace API.DTOs
+public class RegisterRequest
 {
-    public class RegisterRequest
-    {
-        [Required]
-        public required string Username{get;set;} = "";
-        [Required]
-         public required string Password{get;set;} = "";
-    }
+    [Required]
+    public required string Username{get;set;} = string.Empty;
+    [Required]
+    [StringLength(8,MinimumLength =  4)]
+    public required string Password{get;set;} = string.Empty;
 }
