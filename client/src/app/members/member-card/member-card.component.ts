980636f8c7b5ad4830b39aadd1f899507364c418
Antonio_Moreno <gamamurder@gmail.com>
2024-10-29 07:27:50 -0600

    Parcial03 - Add routing to member detail page

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,14 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from '../../models/member';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-member-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './member-card.component.html',
-  styleUrl: './member-card.component.css'
+  styleUrl: './member-card.component.css',
 })
-export class MemberCardComponent {
+export class MemberCardComponent implements OnInit{
+  ngOnInit(): void {
+    console.log("member:"+this.member);
+  }
   member = input.required<Member>();
   
 }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../models/member';
 
 @Component({
   selector: 'app-member-detail',
@@ -7,6 +10,18 @@ import { Component } from '@angular/core';
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent {
-
+export class MemberDetailComponent implements OnInit{
+  private route = inject(ActivatedRoute);
+  private memberService = inject(MembersService);
+  member? : Member;
+  ngOnInit(): void {
+  this.loadMember();
+}
+loadMember(){
+  const username = this.route.snapshot.paramMap.get("username");
+  if(!username) return;
+  this.memberService.getMember(username).subscribe({
+    next:(memberParam)=> this.member = memberParam,
+  });
+}
 }
