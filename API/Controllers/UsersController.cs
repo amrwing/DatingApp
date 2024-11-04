094b696dba994b96a336232d6a191e5e314a0069
Antonio_Moreno <gamamurder@gmail.com>
2024-11-04 07:30:19 -0600

    Parcial03 - Update user repository

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
 namespace API.Controllers;
+
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _repository;
+        private readonly IMapper _mapper;
 
 
     public UsersController(IUserRepository repository)
@@ -35,5 +39,26 @@ public class UsersController : BaseApiController
         }
 
         return member;
+    }
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateRequest request){
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (username == null)
+        {
+            return BadRequest("No username found in token");
+        }
+
+        var user = await _repository.GetByUsernameAsync(username);
+        if(user == null){
+            return BadRequest("Could not find an user");
+        }
+        _mapper.Map(request, user);
+          _repository.Update(user);
+        if(await _repository.SaveAllAsync())
+        {
+            return NoContent();
+        }
+      
+        return BadRequest("Update user failed");
     }
 }
