0b754d2ab3a1cdc040bd3f58ba73a3b8519c63a4
Antonio_Moreno <gamamurder@gmail.com>
2025-02-12 07:12:11 -0600

    Parcial04 - Set main photo in client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,12 +3,13 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../models/member';
 import { of, tap } from 'rxjs';
-
+import { Photo } from '../models/photo';
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   private http = inject(HttpClient);
+  
   baseUrl = environment.apiUrl;
   members = signal<Member[]>([]);
   getMembers(){
@@ -29,6 +30,21 @@ export class MembersService {
         this.members.update((members) => members.map(m=>m.userName === member.userName ? member : m))
       })
     );
+  }
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + "users/photo/" + photo.id, {}).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => {
+          if (m.photos.includes(photo)) {
+            m.photoUrl = photo.url;
+          }
+          return m;
+        }))
+      })
+    );
+  }
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + "users/photo" + "photoId");
   }
   constructor() { }
 }
