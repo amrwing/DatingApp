a3d49313981f17ada7119250006a9cf0b0135d96
Antonio_Moreno <gamamurder@gmail.com>
2025-02-17 08:14:42 -0600

    Parcial04 - Add a reusable text input component

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,12 +2,13 @@ import { Component, inject, input, OnInit, output } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
 import { JsonPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe, NgIf],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
