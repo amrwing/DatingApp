d53903074c4c954f4a92577bed157eb5a841ba54
Antonio_Moreno <gamamurder@gmail.com>
2025-02-17 07:58:40 -0600

    Parcial04 - Delete photo from client

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -65,5 +65,14 @@ export class PhotoEditorComponent implements OnInit {
       }
     });
   }
+  deletePhoto(photo: Photo) {
+    this.memberService.deletePhoto(photo).subscribe({
+      next: _ => {
+        const updatedMember = { ...this.member() };
+        updatedMember.photos = updatedMember.photos.filter(p => p.id !== photo.id);
+        this.memberChange.emit(updatedMember);
+      }
+    });
+  }
 
 }
