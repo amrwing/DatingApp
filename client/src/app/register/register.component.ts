934368f6ac0d6f7a8ac53ee16c7e190d327e5545
Antonio_Moreno <gamamurder@gmail.com>
2025-02-19 07:12:45 -0600

    Parcial04 - Add a reusable date picker component

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -4,11 +4,12 @@ import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { TextInputComponent } from "../_forms/text-input/text-input.component";
 import { JsonPipe, NgIf } from '@angular/common';
+import { DatePickerComponent } from "../_forms/date-picker/date-picker.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent],
+  imports: [ReactiveFormsModule, JsonPipe, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -16,7 +17,7 @@ export class RegisterComponent implements OnInit {
   private fb = inject(FormBuilder);
   ngOnInit(): void {
     this.initializeForm();
-
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
   matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl) => {
@@ -45,6 +46,7 @@ export class RegisterComponent implements OnInit {
   //usersFromHomeComponent = input.required<any>();
   cancelRegister = output<boolean>();
   private toastr = inject(ToastrService);
+  maxDate = new Date();
   model: any = {};
   registerForm: FormGroup = new FormGroup({});
 
