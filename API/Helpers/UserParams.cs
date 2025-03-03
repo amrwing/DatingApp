64134f98ab8028cee44e6f1218306c697dc699d7
Antonio_Moreno <gamamurder@gmail.com>
2025-03-03 07:43:24 -0600

    Parcial05 - Add additional filters

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -15,5 +15,7 @@ public class UserParams
         set => pageSize = (value is < 1 or > MAX_PAGE_SIZE) ? MAX_PAGE_SIZE : value;
                 }
     public string? CurrentUsername { get; set; }
-        public string? Gender { get; set; }
+    public string? Gender { get; set; }
+    public int MinAge { get; set; } = 18;
+    public int MaxAge { get; set; } = 100;
 }
