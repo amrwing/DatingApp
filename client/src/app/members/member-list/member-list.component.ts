e0ca81a8877a89ed89128d7556e697259e22811a
Antonio_Moreno <gamamurder@gmail.com>
2025-03-03 07:39:08 -0600

    Parcial05 - Add client pagination Part 2

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
-import { Member } from '../../models/member';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { Member } from '../../_models/member';
 import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent,MemberCardComponent],
+  imports: [MemberCardComponent,PaginationModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
@@ -22,5 +23,10 @@ export class MemberListComponent implements OnInit{
   }
   loadMembers(){
     this.membersService.getMembers(this.pageNumber, this.pageSize);  }
-
+    pageChanged(event: any) {
+      if (this.pageNumber !== event.page) {
+        this.pageNumber = event.page;
+        this.loadMembers();
+      }
+    }
 }
