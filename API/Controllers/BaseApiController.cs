6f8776efa8ea0bf4636daedcd99abbd5809dcd47
Antonio_Moreno <gamamurder@gmail.com>
2025-03-07 07:25:02 -0600

    Parcial05 - Add ActionFilter

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,8 @@
 namespace API.Controllers;
-using Microsoft.AspNetCore.Mvc;
 
+using API.Helpers;
+using Microsoft.AspNetCore.Mvc;
+[ServiceFilter(typeof(LogUserActivity))]
 [ApiController]
 [Route("api/[controller]")]
 public class BaseApiController : ControllerBase
