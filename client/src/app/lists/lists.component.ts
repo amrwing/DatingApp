3bee18362000998e0796a76a9b502d6af0f1d29b
Antonio_Moreno <gamamurder@gmail.com>
2025-03-17 22:37:22 -0600

    Parcial05 - Add list component to SPA

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MemberCardComponent } from '../members/member-card/member-card.component';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { LikesService } from '../_services/likes.service';
+import { Member } from '../_models/member';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
-  templateUrl: './lists.component.html',
+  imports: [FormsModule, ButtonsModule, MemberCardComponent],  templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
-
+export class ListsComponent implements OnInit {
+  private likesService = inject(LikesService);
+  members: Member[] = [];
+  predicate = "liked";
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+  getTitle() {
+    switch (this.predicate.toLowerCase()) {
+      case "liked": return "Members you like";
+      case "likedby": return "Members who like you";
+      default: return "Mutal like";
+    }
+  }
+  loadLikes() {
+    this.likesService.getLikes(this.predicate).subscribe({
+      next: members => this.members = members
+    })
+  }
 }
