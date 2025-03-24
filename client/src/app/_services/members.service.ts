0b4637914d321e6cccac2e76015c59e71b6e0e71
Antonio_Moreno <gamamurder@gmail.com>
2025-03-24 07:30:06 -0600

    Parcial05 - Add likes pagination to SPA

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -7,35 +7,40 @@ import { Photo } from '../_models/photo';
 import { PaginatedResult } from '../_models/pagination';
 import { UserParams } from '../_models/userParams';
 import { AccountService } from './account.service';
+import { setPaginationHeaders, setPaginationResponse } from './paginationHelper';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   private http = inject(HttpClient);
   private accountService = inject(AccountService);
-    baseUrl = environment.apiUrl;
-  //members = signal<Member[]>([]);
-  membersCache = new Map();
+  baseUrl = environment.apiUrl;
   paginatedResult = signal<PaginatedResult<Member[]> | null>(null);
+  membersCache = new Map();
   user = this.accountService.currentUser();
   userParams = signal<UserParams>(new UserParams(this.user));
-
 
   resetUserParams() {
     this.userParams.set(new UserParams(this.user));
   }
+
   getMembers() {
     let cacheKey = Object.values(this.userParams()).join("-");
     const cachedResponse = this.membersCache.get(cacheKey);
-    if (cachedResponse) return this.setPaginationResponse(cachedResponse);
-    let params = this.setPaginationHeaders(this.userParams().pageNumber, this.userParams().pageSize);
+
+    if (cachedResponse) return setPaginationResponse(cachedResponse, this.paginatedResult);
+
+    let params = setPaginationHeaders(this.userParams().pageNumber, this.userParams().pageSize);
+
     params = params.append("minAge", this.userParams().minAge);
     params = params.append("maxAge", this.userParams().maxAge);
     params = params.append("gender", this.userParams().gender);
     params = params.append("orderBy", this.userParams().orderBy);
+
     return this.http.get<Member[]>(this.baseUrl + "users", { observe: "response", params }).subscribe({
       next: response => {
-        this.setPaginationResponse(response); 
+        setPaginationResponse(response, this.paginatedResult);
         this.membersCache.set(cacheKey, response);
       }
     });
@@ -45,31 +50,21 @@ export class MembersService {
     const member: Member = [...this.membersCache.values()]
       .reduce((arr, elem) => arr.concat(elem.body), [])
       .find((m: Member) => m.userName === username);
+
     if (member) return of(member);
+
     return this.http.get<Member>(this.baseUrl + "users/" + username);
   }
-  private setPaginationResponse(response: HttpResponse<Member[]>) {
-    this.paginatedResult.set({
-      items: response.body as Member[],
-      pagination: JSON.parse(response.headers.get("Pagination")!)
-    });
-  }
-private setPaginationHeaders(pageNumber: number, pageSize: number): HttpParams {
-    let params = new HttpParams();
-    if (pageNumber && pageSize) {
-      params = params.append("pageNumber", pageNumber);
-      params = params.append("pageSize", pageSize);
-    }
-      return params;
-  }
-  updateMember(member: Member){
-    return this.http.put(this.baseUrl+"users",member).pipe(
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + "users", member).pipe(
       tap(() => {
-     //   this.members.update(members => 
-       //   members.map(m => m.userName === member.userName ? member : m))
+        // this.members.update(members => 
+        //   members.map(m => m.userName === member.userName ? member : m))
       })
     );
   }
+
   setMainPhoto(photo: Photo) {
     return this.http.put(this.baseUrl + "users/photo/" + photo.id, {}).pipe(
       // tap(() => {
@@ -82,6 +77,7 @@ private setPaginationHeaders(pageNumber: number, pageSize: number): HttpParams {
       // })
     );
   }
+
   deletePhoto(photo: Photo) {
     return this.http.delete(this.baseUrl + "users/photo/" + photo.id).pipe(
       // tap(() => {
@@ -94,5 +90,4 @@ private setPaginationHeaders(pageNumber: number, pageSize: number): HttpParams {
       // })
     );
   }
-  constructor() { }
 }
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,23 +1,27 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { MemberCardComponent } from '../members/member-card/member-card.component';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { LikesService } from '../_services/likes.service';
-import { Member } from '../_models/member';
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [FormsModule, ButtonsModule, MemberCardComponent],  templateUrl: './lists.component.html',
+  imports: [FormsModule, ButtonsModule, MemberCardComponent, PaginationModule],
+  templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent implements OnInit {
-  private likesService = inject(LikesService);
-  members: Member[] = [];
+export class ListsComponent implements OnInit, OnDestroy {
+  likesService = inject(LikesService);
   predicate = "liked";
+  pageNumber = 1;
+  pageSize = 6;
+
   ngOnInit(): void {
     this.loadLikes();
   }
+
   getTitle() {
     switch (this.predicate.toLowerCase()) {
       case "liked": return "Members you like";
@@ -25,9 +29,19 @@ export class ListsComponent implements OnInit {
       default: return "Mutal like";
     }
   }
+
   loadLikes() {
-    this.likesService.getLikes(this.predicate).subscribe({
-      next: members => this.members = members
-    })
+    this.likesService.getLikes(this.predicate, this.pageNumber, this.pageSize);
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.likesService.paginatedResult.set(null);
   }
 }
