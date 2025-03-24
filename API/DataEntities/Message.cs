edfc5167b4e89f1145c2c83999aa721ba1f77d6e
Antonio_Moreno <gamamurder@gmail.com>
2025-03-24 08:00:15 -0600

    Parcial05 - Add Message Controller

--- API/DataEntities/Message.cs
+++ API/DataEntities/Message.cs
@@ -9,8 +9,8 @@ public class Message
     public required string RecipientUsername { get; set; }
     public required string Content { get; set; }
     public DateTime? DateRead { get; set; }
-    public DateTime MessageSent { get; set; }
-    public bool SenderDeleted { get; set; }
+    public DateTime MessageSent { get; set; } = DateTime.UtcNow;
+        public bool SenderDeleted { get; set; }
     public bool RecipientDeleted { get; set; }
     // EF Navigation properties
     public AppUser Sender { get; set; } = null!;
