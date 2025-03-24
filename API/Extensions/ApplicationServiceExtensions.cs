bd3e6ddb38311a7ddc977d6095354a1daa6936f0
Antonio_Moreno <gamamurder@gmail.com>
2025-03-24 07:55:28 -0600

    Parcial05 - Add Message Repository

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -16,8 +16,10 @@ public static class ApplicationServiceExtensions
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ILikesRepository, LikesRepository>();
         services.AddScoped<LogUserActivity>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+        
         return services;
     }
 }
