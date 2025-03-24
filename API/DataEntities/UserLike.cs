b87f9e8b60781049f5ea92c194877d11590f2f31
Antonio_Moreno <gamamurder@gmail.com>
2025-03-24 07:52:31 -0600

    Parcial05 - Add Message EF Migration

--- API/DataEntities/UserLike.cs
+++ API/DataEntities/UserLike.cs
@@ -7,17 +7,10 @@ public class UserLike
  
 
 {
- 
-
+     // EF Navigation properties
     public AppUser SourceUser { get; set; } = null!;
- 
-
     public int SourceUserId { get; set; }
- 
-
     public AppUser TargetUser { get; set; } = null!;
- 
-
     public int TargetUserId { get; set; }
  
 
