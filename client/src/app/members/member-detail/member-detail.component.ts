5146676a61a2464353b3325f7599b655762a196e
Antonio_Moreno <gamamurder@gmail.com>
2025-05-07 17:56:31 -0600

    Parcial05 - Add message thread to SPA

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -2,36 +2,41 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
-import {TabsModule} from 'ngx-bootstrap/tabs';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
-import { DatePipe } from '@angular/common';
 import { TimeagoModule } from 'ngx-timeago';
+import { DatePipe } from '@angular/common';
+import { MemberMessagesComponent } from "../member-messages/member-messages.component";
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe],
-    templateUrl: './member-detail.component.html',
+  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe, MemberMessagesComponent],
+  templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
 export class MemberDetailComponent implements OnInit{
-  private route = inject(ActivatedRoute);
   private memberService = inject(MembersService);
-  member? : Member;
+  private route = inject(ActivatedRoute);
+  member?: Member;
   images: GalleryItem[] = [];
+  
   ngOnInit(): void {
-  this.loadMember();
-}
-loadMember(){
-  const username = this.route.snapshot.paramMap.get("username");
-  if(!username) return
-  this.memberService.getMember(username).subscribe({
-    next: (member) => {
-      this.member = member;
-      member.photos.map((photo) => {
-        this.images.push(new ImageItem({src: photo.url, thumb: photo.url}));
-      });
-    }
-  });
-}
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get("username");
+    console.log("Username:" + username);
+
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: (member) => {
+        this.member = member;
+        member.photos.map((photo) => {
+          this.images.push(new ImageItem({src: photo.url, thumb: photo.url}));
+        });
+      }
+    });
+  }
 }
