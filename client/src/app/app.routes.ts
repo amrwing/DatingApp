8729419da88b8f261d40fa5e0e9ff27d0fe90f68
Antonio_Moreno <gamamurder@gmail.com>
2025-05-07 18:06:09 -0600

    Parcial05 - Add Route resolver

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -10,29 +10,27 @@ import { NotfoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
+import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
 
-export const routes: Routes = [{
-    path:"",component: HomeComponent},
-   
-        {path:"", runGuardsAndResolvers: "always", 
-            canActivate: [authGuard], children:[    
-                {path: "members", component: MemberListComponent,
-                    canActivate: [authGuard]},
-                    
-                
-            {path:"members/:username",component: MemberDetailComponent},
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {
+        path: "",
+        runGuardsAndResolvers: "always",
+        canActivate: [authGuard],
+        children:[
+            {path: "members", component: MemberListComponent,
+                canActivate: [authGuard]},
+            {path: "members/:username", component: MemberDetailComponent,
+                resolve: { member: memberDetailedResolver }},
             {path: "member/edit", component: MemberEditComponent,
                 canDeactivate: [preventUnsavedChangesGuard]},
-            {path:"lists",component: ListsComponent},
-            
-            {path:"messages",component: MessagesComponent},]
-            
-        },
-
-    {path:"errors",component: TestErrorsComponent},
-    {path:"not-found",component: NotfoundComponent}, 
-    {path:"server-error",component: ServerErrorComponent},
-    {path:"**",component: HomeComponent,pathMatch:"full"},
-    
-
+            {path: "lists", component: ListsComponent},
+            {path: "messages", component: MessagesComponent},
+        ]
+    },
+    {path: "errors", component: TestErrorsComponent},
+    {path: "not-found", component: NotfoundComponent},
+    {path: "server-error", component: ServerErrorComponent},
+    {path: "**", component: HomeComponent, pathMatch: "full"},
 ];
