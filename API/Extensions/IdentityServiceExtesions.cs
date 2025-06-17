b4504709cd8cccb813459fba196c734b5e870989
Antonio_Moreno <gamamurder@gmail.com>
2025-06-16 21:27:45 -0600

    Parcial06 - Add policy based authorization

--- API/Extensions/IdentityServiceExtesions.cs
+++ API/Extensions/IdentityServiceExtesions.cs
@@ -1,17 +1,25 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-
 namespace API.Extensions;
+using System.Text;
+using API.Data;
+using API.DataEntities;
+using API.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 public static class IdentityServiceExtensions
 {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
     {
+        services.AddIdentityCore<AppUser>(opt => opt.Password.RequireNonAlphanumeric = false)
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
             {
-                var tokenKey = config["TokenKey"] ?? throw new ArgumentNullException("TokenKey");
+                var tokenKey = config["TokenKey"] ?? throw new ArgumentException("TokenKey");
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuerSigningKey = true,
@@ -21,6 +29,14 @@ public static class IdentityServiceExtensions
                 };
             });
 
+        services.AddAuthorizationBuilder()
+            .AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"))
+            .AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+
         return services;
     }
+}
+
+internal class AppRole
+{
 }
