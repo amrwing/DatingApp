ef50f07d7b2d95e9419e384647e9ff33bffa7055
Antonio_Moreno <gamamurder@gmail.com>
2025-06-16 22:04:06 -0600

    Parcial06 - Add Presence Tracker with SignalR

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@ namespace API.Extensions;
 using API.Data;
 using API.Helpers;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 public static class ApplicationServiceExtensions
@@ -9,18 +10,22 @@ public static class ApplicationServiceExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
         services.AddControllers();
-        services.AddDbContext<DataContext>(opt => opt.UseSqlite(config.GetConnectionString("DefaultConnection")));
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
-        services.AddScoped<IPhotoService,PhotoService>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ILikesRepository, LikesRepository>();
-        services.AddScoped<LogUserActivity>();
         services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<IPhotoService, PhotoService>();
+        services.AddScoped<LogUserActivity>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
         services.AddSignalR();
-                
+        services.AddSingleton<PresenceTracker>();
+
         return services;
     }
 }
